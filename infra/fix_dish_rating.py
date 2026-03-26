d5051adc032d73db08342060830279552ed437de
Petrushaa <logacevz@gmail.com>
2026-03-26 20:24:33 +0300

    готовый проект

--- infra/fix_dish_rating.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-with open('/home/jeka/foodyFront/frontend/src/app/(main)/dish/[id]/page.tsx', 'r') as f:
-    text = f.read()
-
-text = text.replace('const ratingScore = Number(dish.userRating || 0).toFixed(1);', 'const ratingScore = Math.round(dish.userRating || 0);')
-
-with open('/home/jeka/foodyFront/frontend/src/app/(main)/dish/[id]/page.tsx', 'w') as f:
-    f.write(text)
